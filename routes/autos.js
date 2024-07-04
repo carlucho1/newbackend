e7d5c5f9cc50be77fcb771a668ce16d6f698b222
Carlos Binker <carlosbinker@gmail.com>
2024-07-04 09:16:13 -0300

    Se incorporó el método PUT. El mismo se activa desde la planilla update y se le pasa el id como parámetro. En el backend en la URI se lo recibe como :id

--- routes/autos.js
+++ routes/autos.js
@@ -9,12 +9,12 @@ const fs = require("fs");
 
 /* GET tablaautos -- Obtengo tablaautos en formato JSON en el Front End */
 router.get("/tablaautos", function (req, res, next) {
-    let query = "select * from tablaautos";
-    db.query(query, function (error, results, fields) {
-        if (error) throw error;
-        res.json({data: results});
-        console.log(results);
-    });
+  let query = "select * from tablaautos";
+  db.query(query, function (error, results, fields) {
+    if (error) throw error;
+    res.json({ data: results });
+    console.log(results);
+  });
 });
 
 /* GET tablaimages -- Obtengo tablaimages en formato JSON en el Front End */
@@ -40,15 +40,25 @@ router.get("/", function (req, res, next) {
     db.query(query2, (err, results2) => {
       if (err) throw err;
 
-      res.render("index", {data1: results1, data2: results2});
+      res.render("index", { data1: results1, data2: results2 });
     });
   });
 });
 
 //Ruta para insertar un registro en la tabla tablaautos
 router.post("/", function (req, res, next) {
- 
-  let query = 'insert into tablaautos (marca, modelo, año, precio, color) values("' + req.body.marca + '","' + req.body.modelo + '",' + req.body.año + ',' + req.body.precio + ',"' + req.body.color + '")';
+  let query =
+    'insert into tablaautos (marca, modelo, año, precio, color) values("' +
+    req.body.marca +
+    '","' +
+    req.body.modelo +
+    '",' +
+    req.body.año +
+    "," +
+    req.body.precio +
+    ',"' +
+    req.body.color +
+    '")';
 
   db.query(query, function (error, results, fields) {
     if (error) throw error;
@@ -59,66 +69,94 @@ router.post("/", function (req, res, next) {
 });
 
 /* Dashboard de Productos */
-router.get('/listado/', function(req, res, next) {
-
+router.get("/listado/", function (req, res, next) {
   // En el objeto query viene lo que se pasa por GET en línea de comandos
-  if(req.query.id){
-      query = 'select * from autosimages where id = ' +  req.query.id
+  if (req.query.id) {
+    query = "select * from autosimages where id = " + req.query.id;
   } else {
-    query = 'select * from autosimages' 
+    query = "select * from autosimages";
   }
 
   db.query(query, function (error, results, fields) {
-
-      if (error) throw error;
-      // res.json({data: results})
-      res.render('dashboard', {data:results});
-    });
-
+    if (error) throw error;
+    // res.json({data: results})
+    res.render("dashboard", { data: results });
+  });
 });
 
+// Formulario de alta
 router.get("/alta", function (req, res, next) {
   res.render("alta");
 });
 
 // Ruta con el alta de producto
-router.post("/alta/", upload.single("urlImage"), async function (req, res, next) {
-  // Concatenando cadenas con signo +
+router.post(
+  "/alta/",
+  upload.single("urlImage"),
+  async function (req, res, next) {
+    // Concatenando cadenas con signo +
 
-  let sentencia =
-    'insert into autosimages (urlImage, descripcion, precio) values("'+'/images/'+
-    req.file.originalname +
-    '","' +
-    req.body.descripcion +
-    '","' +
-    req.body.precio +
-    '")';
+    let query =
+      'insert into autosimages (urlImage, descripcion, precio) values("' +
+      "/images/" +
+      req.file.originalname +
+      '","' +
+      req.body.descripcion +
+      '","' +
+      req.body.precio +
+      '")';
 
-  // Usando template string
+    // Usando template string
 
-  // `insert into productos(nombre, descripcion,  imagen) values('${req.body.nombre}','${req.body.descripcion}','/images/${req.file.originalname}')`
+    // `insert into productos(nombre, descripcion,  imagen) values('${req.body.nombre}','${req.body.descripcion}','/images/${req.file.originalname}')`
 
-  let results = await db.query(sentencia);
+    let results = await db.query(query);
 
-  fs.createReadStream("./uploads/" + req.file.filename).pipe(
-    fs.createWriteStream("./public/images/" + req.file.originalname),
-    function (error) {}
+    fs.createReadStream("./uploads/" + req.file.filename).pipe(
+      fs.createWriteStream("./public/images/" + req.file.originalname),
+      function (error) {}
+    );
+
+    res.render("finalizado", { mensaje: "Producto Ingresado Exitosamente" });
+  }
+);
+
+// Ruta para editar el producto
+router.get("/update/:id", function (req, res, next) {
+  db.query(
+    "select * from autosimages where id = " + req.params.id,
+    function (error, results, fields) {
+      if (error) throw error;
+      // res.json({data: results})
+      res.render("update", { data: results });
+    }
   );
-
-  res.render("finalizado", { mensaje: "Producto Ingresado Exitosamente" });
 });
 
+router.post("/update/:id",upload.single("urlImage"), async function (req, res, next) {
+    // Concatenando cadenas con signo +
 
-// Ruta para mostrar el formulario de actualización
-router.get('/edit/:id', (req, res) => {
-  const id = req.params.id;
-  const query = 'SELECT * FROM tablaautos WHERE id = ?';
+    let query;
 
-  db.query(query, [id], (err, results) => {
-    if (err) throw err;
-    res.render('update', { auto: results[0] });
-  });
-});
+if (req.file) {
+  query = `update autosimages set urlImage  = '/images/${req.file.originalname}, 
+  descripcion = '${req.body.descripcion}', precio = '${req.body.precio}' where id = ${req.params.id}`;
 
+      fs.createReadStream("./uploads/" + req.file.filename).pipe(
+        fs.createWriteStream("./public/images/" + req.file.originalname),
+        function (error) {}
+      );
+    } else {
+      query = `update autosimages set descripcion  = '${req.body.descripcion}', precio  = '${req.body.precio}' where id = ${req.params.id}`;
+    }
+
+    db.query(query, function (error, results, fields) {
+      if (error) throw error;
+      // res.json({data: results})
+      res.render("finalizado", {mensaje: "El producto fue modificado exitosamente",
+      });
+    });
+  }
+);
 
 module.exports = router;
